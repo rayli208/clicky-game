4d1519b6115e500af27aa9cff4e76d922bd95f29
Rayli Raykov <rayli208@gmail.com>
2019-03-06 10:31:49 -0500

    make it not private

---
+++ build/precache-manifest.204639c852bde6083f626a73604b6ee9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "4db3e9c20bb2096cc755",
+    "url": "/clicky-game/static/js/main.4db3e9c2.chunk.js"
+  },
+  {
+    "revision": "c327e0905abffeb7ddf0",
+    "url": "/clicky-game/static/js/2.c327e090.chunk.js"
+  },
+  {
+    "revision": "0b7af0b0c4b7b82c2e1faff697e37946",
+    "url": "/clicky-game/index.html"
+  }
+];
